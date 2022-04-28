dea7016ab4d0b492ec62904b76688bac31d5c9ae
jonnyalexbh <jonnyalex.bh@gmail.com>
2022-04-28 16:05:13 -0500

    Separate routes from the app.js

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import * as routes from './src/routes';
 
 const app = express();
 
-app.get('/', (_, res) => {
-  res.status(200).send({ message: 'Hello World' })
-});
+routes.init(app);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
