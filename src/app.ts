2a8b0d2db7b59b1a8c815c114d14eccb688b3535
jonnyalexbh <jonnyalex.bh@gmail.com>
2025-09-12 09:04:38 -0500

    Extract routes into separate module from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
